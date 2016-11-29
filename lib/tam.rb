f138fcca28bedefa0efd097a70203783afd0443a
Devin Ludwig and Kevin Finley <devin@>
2016-11-29 11:50:20 -0800

    add spec for time passing and food level reduction with passing methods.

--- lib/tam.rb
+++ lib/tam.rb
@@ -1,10 +1,13 @@
 class Tamagotchi
+  @@last_time = Time.new
+  @@burned_calories = 0
 
   def initialize(pet_name, food=10, sleep_lvl=10, activity_level=10)
     @pet_name = pet_name
     @food = food
     @sleep = sleep_lvl
     @activity_level = activity_level
+    @birth_time = Time.new
   end
 
   def name
@@ -29,5 +32,19 @@ class Tamagotchi
     else
       true
     end
+  end
+
+  def set_food_level(level)
+    @food = level
+  end
+
+  define_singleton_method(:time_passes) do
+    current_time = Time.new
+    @@burned_calories = ((current_time - @@last_time)/10).ceil
+    @@last_time = current_time
+  end
+
+  def burn_calories
+    @food -= @@burned_calories
   end
 end
--- spec/tam_spec.rb
+++ spec/tam_spec.rb
@@ -17,5 +17,20 @@ describe(Tamagotchi) do
       yoko = Tamagotchi.new('yoko')
       expect(yoko.is_alive()).to(eq(true))
     end
+
+    it('is dead if the food level is 0') do
+      yoko = Tamagotchi.new('yoko')
+      yoko.set_food_level(0)
+      expect(yoko.is_alive()).to(eq(false))
+    end
+  end
+
+  describe('#time_passes') do
+    it "updates last_time and burned_calories based on how much time has passed since method last ran" do
+      my_pet = Tamagotchi.new('whatever')
+      Tamagotchi.time_passes()
+      my_pet.burn_calories()
+      expect(my_pet.food()).to(eq(9))
+    end
   end
 end
